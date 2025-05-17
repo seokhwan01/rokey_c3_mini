b2ecc9f3c0567795a2d2fa3114c8fd18b600f0c3
afleejw <149562149+afleejw@users.noreply.github.com>
2025-05-17 13:39:30 +0900

    Add files via upload

---
+++ src/detection/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+import glob
+import os
+
+
+package_name = 'detection'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob.glob(os.path.join('launch', '*.launch.py'))),
+        ('share/' + package_name + '/param', glob.glob(os.path.join('param', '*.yaml'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='choi',
+    maintainer_email='choi@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'detection_manager = detection.detectionManager:main',
+            'display_manager = detection.displayManager:main',
+            'tracking_manager = detection.trackingManager:main',
+            'capture_image = detection.ImagePublisher:main',
+            'qr_detection = detection.QRDetection:main',
+        ],
+    },
+)
