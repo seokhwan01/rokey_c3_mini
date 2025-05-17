9b778e43818788a56194d9a0dbb3303e62a62e0d
afleejw <149562149+afleejw@users.noreply.github.com>
2025-05-17 18:39:53 +0900

    Update setup.py

--- src/controller/setup.py
+++ src/controller/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'goal_manager: controller.goal_manager:main',
+            'goal_manager = controller.goal_manager:main',
         ],
     },
 )
